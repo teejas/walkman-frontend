ffe79e7f98b78b9882a3ee2cea9639a822209236
Tejas Siripurapu <tejassiripurapu@tejass-mbp.lan>
2022-07-17 20:35:23 -0700

    update spotify player and get recommendations

--- src/App.js
+++ src/App.js
@@ -1,69 +1,41 @@
 import Login from './Login';
 import React, { Component, useCallback, useEffect }  from 'react';
 import { Box, Button, Typography, Grid } from '@material-ui/core';
-import { Stack, HStack, VStack } from '@chakra-ui/react'
-import { WebPlaybackSDK, usePlaybackState, useWebPlaybackSDKReady, useSpotifyPlayer } from "react-spotify-web-playback-sdk";
+import { Stack, HStack, VStack } from '@chakra-ui/react';
+import SpotifyWebPlayer from "react-spotify-web-playback"
 import axios from 'axios';
 
-const MyPlayer = () => {
-  const webPlaybackSDKReady = useWebPlaybackSDKReady();
-
-  if (!webPlaybackSDKReady) return <div>Loading...</div>;
-
-  return <Typography>Spotify App is ready!</Typography>;
-};
-
-const SongTitle = () => {
-  const playbackState = usePlaybackState();
-
-  if (playbackState === null) return null;
-
-  return <Typography>Current song: {playbackState.track_window.current_track.name}</Typography>;
-};
-
-const PauseResumeButton = () => {
-  const player = useSpotifyPlayer();
-
-  console.log(player)
-
-  if (player === null) return null;
-
+const MySpotifyPlayer = (props) => {
+  console.log("URIS: ", props.uris)
   return (
-    <div>
-      <Button variant="contained" color="primary" onClick={() => player.pause()}>pause</Button>
-      <Button variant="contained" color="primary" onClick={() => player.resume()}>resume</Button>
-    </div>
+    <SpotifyWebPlayer
+      initialVolume={50}
+      persistDeviceSelection
+      play={true}
+      showSaveIcon
+      styles={{
+        sliderColor: '#1cb954',
+      }}
+      syncExternalDevice
+      token={props.code}
+      uris={props.uris}
+    />
   );
 };
 
-function MySpotifyPlayer(props) {
-  const getOAuthToken = useCallback(callback => callback(props.code), []);
-
-  return (
-    <WebPlaybackSDK
-      deviceName="Walkman Spotify App"
-      getOAuthToken={getOAuthToken}
-      volume={0.5}
-      connectOnInitialized={true}>
-        <MyPlayer />
-        <SongTitle />
-        <PauseResumeButton />
-    </WebPlaybackSDK>
-  );
-};
-
-function App(props) {
+const App = (props) => {
   
   const code = new URLSearchParams(window.location.search).get('code')
   const [token, setToken] = React.useState(null)
+  const [uris, setUris] = React.useState([])
 
-  useEffect(() => {
+  const fetchToken = () => {
     var bodyFormData = new FormData();
     bodyFormData.append('code', code);
     bodyFormData.append('redirect_uri', 'http://localhost:3000/');
     bodyFormData.append('grant_type', 'authorization_code');
     var authOptions = {
-      url: 'http://localhost:8000/api/auth/spotify/',
+      url: 'http://localhost:8000/api/spotify/get_token/',
       method: 'POST',
       data: bodyFormData,
       headers: {
@@ -74,28 +46,59 @@ function App(props) {
     console.log("authOptions: ", authOptions)
     axios(authOptions)
     .then(function (response) {
-      console.log("response: ", response)
       setToken(response.data)
     })
     .catch(function (error) {
       console.log(error);
     });
+  }
+
+  const fetchRecommendations = () => {
+    var authOptions = {
+      url: 'http://localhost:8000/api/spotify/get_recommendations/',
+      method: 'GET',
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    };
+
+    axios(authOptions)
+    .then(function (response) {
+      console.log("response: ", response)
+      setUris(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+  useEffect(() => {
+    if (!token) {
+      fetchToken();
+    }
+    if (uris.length === 0) {
+      fetchRecommendations();
+    }
   })
 
   if(token) {
-    console.log("token: ", token)
-    return (
-      <Grid  
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Grid item xs>
-          <MySpotifyPlayer code={token}/>
+    if(uris.length > 0) {
+      return (
+        <Grid  
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs>
+            <MySpotifyPlayer code={token} uris={uris} />
+          </Grid>
         </Grid>
-      </Grid>
-    );
+      );
+    } else {
+      return <Typography>Loading URI's...</Typography>
+    }
   } else {
     return (
       <Login />
--- src/spotify.js
+++ src/spotify.js
@@ -3,14 +3,25 @@ const redirectUri = "http://localhost:3000/";
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 const scopes = [
-    "streaming",
-    "user-read-email",
-    "user-read-private",
-    "app-remote-control",
-    "playlist-read-collaborative",
-    "playlist-modify-public",
-    "playlist-read-private",
-    "playlist-modify-private",
+    'ugc-image-upload',
+    'user-modify-playback-state',
+    'user-read-playback-state',
+    'user-read-currently-playing',
+    'user-follow-modify',
+    'user-follow-read',
+    'user-read-recently-played',
+    'user-read-playback-position',
+    'user-top-read',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'app-remote-control',
+    'streaming',
+    'user-read-email',
+    'user-read-private',
+    'user-library-modify',
+    'user-library-read',
 ];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
